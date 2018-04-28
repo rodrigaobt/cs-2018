ca82cf8b9b430376e11bbda6886d82a377cd29c4
Rodrigo Barbosa <e.rodrigobarbosa@gmail.com>
2018-04-28 12:31:50 -0300

    Add files via upload

---
+++ topico-2/tipo-dinamico.js
@@ -0,0 +1,9 @@
+/* 
+O tipo de uma variável em JavaScript é dinâmico, ou seja, pode ser alterado, conforme abaixo.
+*/
+// O tipo de uma variável em JavaScript pode ser alterado durante a execução
+let x = "Mensagem";
+console.log("x é do tipo " + (typeof x));
+
+x = 9.8;
+console.log("x é do tipo " + (typeof x));
